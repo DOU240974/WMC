771d740c55fddee10dcbe956700fd84250807c4c
DOU <dou240974@spengergasse.at>
2026-04-16 19:38:30 +0200

    Hü update

---
+++ 20260326 HÜ2/brief.js
@@ -0,0 +1,37 @@
+// Brief holen
+function holeBrief(inhalt) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(inhalt);
+        }, 1000);
+    });
+}
+
+// Brief stempeln
+function stempelBrief(brief) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(brief + " [Gestempelt]");
+        }, 1000);
+    });
+}
+
+// Brief versenden
+function versendeBrief(brief) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(brief + " -> Versendet!");
+        }, 1000);
+    });
+}
+
+// Promise-Chaining
+holeBrief("Guck mal Brief ist da!")
+    .then((brief) => stempelBrief(brief))
+    .then((brief) => versendeBrief(brief))
+    .then((fertigerBrief) => {
+        console.log(fertigerBrief);
+    })
+    .catch((error) => {
+        console.error("Fehler:", error);
+    });
